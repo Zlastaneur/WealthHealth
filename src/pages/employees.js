2fb4b51685e18f5588deea375959370954bd851a
Stanley <stanley.defer@hotmail.Fr>
2025-05-19 23:13:11 +0200

    💡 Update README

--- src/pages/employees.js
+++ src/pages/employees.js
@@ -4,16 +4,16 @@ import { useSelector, useDispatch } from "react-redux"
 import DataTable from "../components/dataTable.js"
 import { useEffect } from "react"
 import { initMockEmployees } from "../features/employeeSlice"
-import mockEmployees from "../data/mock-employees.json";
+import mockEmployees from "../data/mock-employees.json"
 
 function Employees() {
-	const employees = useSelector((state) => state.employees.data);
-	const dispatch = useDispatch();
+	const employees = useSelector((state) => state.employees.data)
+	const dispatch = useDispatch()
 	useEffect(() => {
-		if(employees.length < 2) {
+		if (employees.length < 2) {
 			dispatch(initMockEmployees([...mockEmployees, ...employees]))
 		}
-	}, [employees]);
+	}, [employees, dispatch])
 
 	return (
 		<div id="employee-div" className="employee-container">
